62d221b5946a2d16688574532c9a16bb4889d356
vaxradius <vaxradius@gmail.com>
2021-07-03 22:59:32 +0800

    Initiate cooper power test

---
+++ boards/apollo4b_eb/examples/validation/cooper_power_test/src/ble_firmware_update.c
@@ -0,0 +1,202 @@
+//*****************************************************************************
+//
+//! @file ble_firmware_update.c
+//!
+//! @brief This is the application just for updating built-in BLE firmware into Cooper.
+//!
+//!
+//
+//*****************************************************************************
+
+//*****************************************************************************
+//
+// Copyright (c) 2021, Ambiq Micro, Inc.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+// this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its
+// contributors may be used to endorse or promote products derived from this
+// software without specific prior written permission.
+//
+// Third party software included in this distribution is subject to the
+// additional license terms as defined in the /docs/licenses directory.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+// This is part of revision b0-release-20210111-375-gc3201eeb4 of the AmbiqSuite Development Package.
+//
+//*****************************************************************************
+
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
+#include "am_mcu_apollo.h"
+#include "am_bsp.h"
+#include "am_util.h"
+#include "am_devices_cooper.h"
+#include "ble_fw_image.h"
+
+//*****************************************************************************
+//
+// Configuration options
+//
+//*****************************************************************************
+
+//*****************************************************************************
+//
+// SPI configuration.
+//
+//*****************************************************************************
+
+
+//*****************************************************************************
+//
+// Global Variables
+//
+//*****************************************************************************
+uint32_t DMATCBBuffer[2048];
+
+void *g_IomDevHdl;
+void *g_pvHciSpiHandle;
+
+
+#define COOPER_IOM_IRQn           ((IRQn_Type)(IOMSTR0_IRQn + SPI_MODULE))
+
+
+#define APOLLO4_SIP
+#define AM_DEVICES_COOPER_RESET_PIN     42
+
+
+//*****************************************************************************
+//
+// Main function.
+//
+//*****************************************************************************
+int
+enter_deepsleep(void)
+{
+
+	am_hal_pwrctrl_mcu_memory_config_t McuMemCfg =
+    {
+      .eCacheCfg    = AM_HAL_PWRCTRL_CACHE_NONE,
+      .bRetainCache = false,
+      .eDTCMCfg     = AM_HAL_PWRCTRL_DTCM_128K,
+      .eRetainDTCM  = AM_HAL_PWRCTRL_DTCM_8K,
+      .bEnableNVM0  = true,
+      .bRetainNVM0  = false
+    };
+
+    am_hal_pwrctrl_sram_memcfg_t SRAMMemCfg =
+    {
+      .eSRAMCfg         = AM_HAL_PWRCTRL_SRAM_NONE,
+      .eActiveWithMCU   = AM_HAL_PWRCTRL_SRAM_NONE,
+      .eActiveWithDSP   = AM_HAL_PWRCTRL_SRAM_NONE,
+      .eSRAMRetain      = AM_HAL_PWRCTRL_SRAM_NONE
+    };
+
+    //
+    // Set the default cache configuration
+    //
+    am_hal_cachectrl_config(&am_hal_cachectrl_defaults);
+    am_hal_cachectrl_enable();
+		
+    //
+    // Configure the board for low power.
+    //
+    //am_bsp_low_power_init();
+	//PWRCTRL->VRCTRL_b.SIMOBUCKEN = 0;
+
+    //
+    // Disable XTAL in deepsleep
+    //
+    am_hal_pwrctrl_control(AM_HAL_PWRCTRL_CONTROL_XTAL_PWDN_DEEPSLEEP, 0);
+
+    //
+    // Update memory configuration to minimum.
+    //
+    am_hal_pwrctrl_mcu_memory_config(&McuMemCfg);
+    am_hal_pwrctrl_sram_config(&SRAMMemCfg);
+
+
+    while (1)
+    {
+        //
+        // Go to Deep Sleep and stay there.
+        //
+        am_hal_sysctrl_sleep(AM_HAL_SYSCTRL_SLEEP_DEEP);
+    }
+}
+
+
+//*****************************************************************************
+//
+// Main
+//
+//*****************************************************************************
+int
+main(void)
+{
+	uint32_t ui32Status;
+	am_devices_cooper_config_t stCooperConfig;
+	stCooperConfig.ui32ClockFreq = COOPER_IOM_FREQ;
+	stCooperConfig.pNBTxnBuf = DMATCBBuffer;
+	stCooperConfig.ui32NBTxnBufLength = sizeof(DMATCBBuffer) / 4;
+
+    //
+    // Default setup.
+    //
+    am_bsp_low_power_init();
+
+    //
+    // Initialize the SPI module.
+    //
+    ui32Status = am_devices_cooper_init(SPI_MODULE, &stCooperConfig, &g_IomDevHdl, &g_pvHciSpiHandle);
+    if ( ui32Status )
+    {
+        while(1);
+    }
+
+
+	while(am_devices_cooper_clkreq_read(g_IomDevHdl))
+	{
+
+	}
+
+	am_util_delay_ms(100); // For cooper sleep(idle) abnormal part , that needs at least 40ms
+
+#if (!defined(COOPER_QFN))
+    am_hal_gpio_pinconfig(AM_BSP_GPIO_IOM4_CS,   am_hal_gpio_pincfg_disabled);// For cooper sleep(idle) abnormal part
+#else
+	am_hal_gpio_pinconfig(AM_BSP_GPIO_IOM0_CS,   am_hal_gpio_pincfg_disabled);// For cooper sleep(idle) abnormal part
+#endif
+
+	 am_hal_mcuctrl_control(AM_HAL_MCUCTRL_CONTROL_EXTCLK32K_DISABLE, 0);    
+	 am_hal_mcuctrl_control(AM_HAL_MCUCTRL_CONTROL_EXTCLK32M_DISABLE, 0);             
+	 //am_hal_pwrctrl_control(AM_HAL_PWRCTRL_CONTROL_CRYPTO_POWERDOWN, 0);    //    // Disable all peripherals    //    
+	 //am_hal_pwrctrl_control(AM_HAL_PWRCTRL_CONTROL_DIS_PERIPHS_ALL, 0);    //    // Disable XTAL in deepsleep    //    
+	 am_hal_pwrctrl_control(AM_HAL_PWRCTRL_CONTROL_XTAL_PWDN_DEEPSLEEP, 0);
+
+
+
+    enter_deepsleep();
+
+}
